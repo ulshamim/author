ae9e5d4d5ace8101a644c5dee9a8cd0d9d89585d
shamim <shamim@itobuz.com>
2015-01-19 18:17:12 +0530

    Alignment changes
    Some Alignment chnages

--- app/controllers/LoginController.php
+++ app/controllers/LoginController.php
@@ -1,57 +1,52 @@
-<?php 
+<?php
+
 use Redooor\Redminportal\User;
 
-class LoginController extends BaseController
-{
+class LoginController extends BaseController {
+
     protected $model;
 
-    public function __construct(User $user)
-    {
+    public function __construct(User $user) {
         $this->model = $user;
     }
 
-    public function getIndex()
-    {
+    public function getIndex() {
         return View::make('users/login');
     }
 
-    public function getUnauthorized()
-    {
+    public function getUnauthorized() {
         return View::make('redminportal::users/notauthorized');
     }
 
-    public function getLogout()
-    {
+    public function getLogout() {
         // Logs the user out
         Sentry::logout();
         return Redirect::to('/');
     }
 
-    public function postLogin()
-    {
+    public function postLogin() {
         /*
          * Validate
          */
         $rules = array(
-            'email'     => 'required|email',
-            'password'  => 'required',
+            'email' => 'required|email',
+            'password' => 'required',
         );
 
         $validation = Validator::make(\Input::all(), $rules);
 
         if ($validation->passes()) {
-            $email      = Input::get('email');
-            $password   = Input::get('password');
+            $email = Input::get('email');
+            $password = Input::get('password');
 
             try {
                 $credentials = array(
-                    'email'    => $email,
+                    'email' => $email,
                     'password' => $password,
                 );
 
                 // Authenticate the user
                 Sentry::authenticate($credentials, false);
-                
             } catch (Cartalyst\Sentry\Users\UserNotActivatedException $e) {
                 $errors = new \Illuminate\Support\MessageBag;
                 $errors->add('invalid', "This user hasn't been activated. Please contact us for support.");
@@ -63,10 +58,14 @@ class LoginController extends BaseController
 
                 return Redirect::to('users/login')->withErrors($errors)->withInput();
             }
-
-            return Redirect::to('/');
+            if (Sentry::getUser()->hasAccess('admin')) {
+                return Redirect::to('/admin');
+            } else {
+                return Redirect::to('/');
+            }
         }
 
         return Redirect::to('users/login')->withErrors($validation)->withInput();
     }
+
 }
